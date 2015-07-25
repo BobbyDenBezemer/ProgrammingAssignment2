a8cbe2867f517ca27033a169b2a3f50aae91c6e4
BobbyXiao <bobbydenbezemer@gmail.com>
2015-07-25 16:52:21 +0200

    Adding cacheMatrix R file and examples

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Function initializes a CacheMatrix object and its methods
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  # A set function to set a cacheMatrix object
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # A get function to get a cacheMatrix object
+  get <- function() x
+  
+  # a set function to set the inverse of a cacheMatrix object
+  setInv <- function(mat) inv <<- mat
+  
+  # a get function to get the inverse of a cacheMatrix object
+  getInv <- function() inv
+  
+  # returns the objects methods as a list
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of a cacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get the inverse of a cache matrix
+  inv <- x$getInv()
+  
+  # if the inverse is already intialized, return it
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # if the inverse is not intialized, get the cacheMatrix object
+  data <- x$get()
+  
+  # calculate the inverse of the cacheMatrix object
+  inv <- solve(data, ...)
+  
+  # set the inverse of the cacheMatrix object to the calculated inverse
+  x$setInv(inv)
+  
+  # return the inverse of the cacheMatrix object
+  inv
 }
+
+## Test example
+# initialize a cacheMatrix
+testCacheMatrix <- makeCacheMatrix(x = matrix(rnorm(9), nrow = 3, ncol = 3))
+
+# get the cacheMatrix
+testCacheMatrix$get()
+
+# calculate the inverse and print it
+cacheSolve(testCacheMatrix)
+
+# get the inverse from the cacheMatrix
+testCacheMatrix$getInv()
